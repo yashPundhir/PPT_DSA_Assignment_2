a710de47074707f9ec1544e46bd88c856df44541
yashPundhir <yash.pundhir.prof@gmail.com>
2023-06-01 16:43:38 +0530

    ques 7 solved

--- sol.js
+++ sol.js
@@ -63,3 +63,36 @@ function search(nums, target) {
 	}
 	return -1;
 }
+
+/*
+
+Question 7
+
+An array is monotonic if it is either monotone increasing or monotone decreasing.
+
+An array nums is monotone increasing if for all i <= j, nums[i] <= nums[j]. An array nums is
+monotone decreasing if for all i <= j, nums[i] >= nums[j].
+
+Given an integer array nums, return true if the given array is monotonic, or false otherwise.
+
+Example 1:
+Input: nums = [1,2,2,3]
+Output: true
+
+*/
+
+// Solution 7
+
+function checkMonotonic(nums) {
+	for (let i = 0; i < nums.length - 1; i++) {
+		if (nums[i] > nums[i + 1]) {
+			return false;
+		}
+	}
+	for (let i = 0; i < nums.length - 1; i++) {
+		if (nums[i] < nums[i + 1]) {
+			return false;
+		}
+	}
+	return true;
+}
